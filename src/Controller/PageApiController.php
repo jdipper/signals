7b1d62ca4ab476b984d59f00999a9a5fc83d2d15
Jack Dipper <14117126+jdipper@users.noreply.github.com>
2018-04-11 23:07:13 +0100

    Added Use PageType

--- src/Controller/PageApiController.php
+++ src/Controller/PageApiController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Page;
+use App\Form\PageType;
 
 class PageApiController extends Controller
 {
