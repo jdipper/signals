404e609081bbe4a934a14e0175dc086eaa8544bf
Jack <14117126+jdipper@users.noreply.github.com>
2019-06-26 20:38:04 +0100

    Updatd base.html.twig for front page view

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,14 @@ require('../css/app.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
